687203c2eb1caab70731c7c9bf2c870bd5d6f2e4
Hedonium <hedonizing@gmail.com>
2023-12-29 05:32:09 -0500

    Adding more sections

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 
+### 3 trucks
+### 2 drivers
+### Drivers leave the hub no earlier than 8:00 a.m
+### The delivery address for package #9, Third District Juvenile Court, is wrong and will be corrected at 10:20 a.m. WGUPS is aware that the address is incorrect and will be updated at 10:20 a.m. However, WGUPS does not know the correct address (410 S. State St., Salt Lake City, UT 84111) until 10:20 a.m.
+
+### 40 packages for two of the trucks
+### Under 140 miles
+
 # Package Class
+#
 
 class Package:
     def __init__(self, package_id, address, deadline, city, zipcode, weight, delivery_status, delivery_time):
@@ -16,6 +25,7 @@ class Package:
         return f"Package - Address: {self.address}, Deadline: {self.deadline}, City: {self.city}, Zipcode: {self.zipcode}, Weight: {self.weight} kg, Status: {self.status}, Time: {self.time}"
 
 # Truck Class
+#
 
 class Truck:
     def ___init__(self, truck_id, packages, package_limit = 16, miles_driven, location):
@@ -25,13 +35,22 @@ class Truck:
         self.miles_driven = miles_driven
         self.location = location
 
+def __str__(self):
+        return f"Truck ID: {self.truck_id}, Packages: {self.packages}, Package Limit: {self.package_limit}, Miles Driven: {self.miles_driven}, Location: {self.location}"
+
     def add(self, package):
         pass
 
     def remove(self, package):
         pass
 
+# CSV Reading
+#
+
+
+
 # Hash Table
+#
 
 class HashTable:
     def __init__(self):
@@ -46,12 +65,16 @@ class HashTable:
     def search(self, key):
         pass
 
-# Nearest_Neighbor
+# Nearest_Neighbor Algorithm
+#
 
 def nearest():
     pass:
 
-# Main and User Interface
+# Main with User Interface
+# 1. Provide screenshots to show the status of all packages loaded onto each truck at a time between 8:35 a.m. and 9:25 a.m.
+# 2. Provide screenshots to show the status of all packages loaded onto each truck at a time between 9:35 a.m. and 10:25 a.m.
+# 3. Provide screenshots to show the status of all packages loaded onto each truck at a time between 12:03 p.m. and 1:12 p.m.
 
 if __name__ == "__main__":
     # Mohmoud Mohamed - 002439508    
