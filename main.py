f06e34282794e059703b00d82c021162e50882eb
Hedonium <hedonizing@gmail.com>
2023-12-29 04:22:48 -0500

    Filling out more of the framework

--- main.py
+++ main.py
@@ -2,37 +2,58 @@
 # Package Class
 
 class Package:
-    def __init__(self, address, deadline, city, zipcode, weight):
-        self.ID = ID
+    def __init__(self, package_id, address, deadline, city, zipcode, weight, delivery_status, delivery_time):
+        self.package_id = package_id
         self.address = address
         self.deadline = deadline
         self.city = city
         self.zipcode = zipcode
         self.weight = weight
-        self.status = status
-        self.time = time
+        self.delivery_status = delivery_status
+        self.delivery_time = delivery_time
 
     def __str__(self):
         return f"Package - Address: {self.address}, Deadline: {self.deadline}, City: {self.city}, Zipcode: {self.zipcode}, Weight: {self.weight} kg, Status: {self.status}, Time: {self.time}"
 
 # Truck Class
 
+class Truck:
+    def ___init__(self, truck_id, packages, package_limit = 16, miles_driven, location):
+        self.truck_id = truck_id
+        self.packages = []
+        self.package_limit = package_limit
+        self.miles_driven = miles_driven
+        self.location = location
 
+    def add(self, package):
+        pass
+
+    def remove(self, package):
+        pass
 
 # Hash Table
 
-class HashMap:
-    pass
+class HashTable:
+    def __init__(self):
+        self.table = []
 
-    def insert(self, id):
+    def insert(self, key):
         pass
 
-    def search(self, id):
+    def delete(self, key):
         pass
 
-# User Interface
+    def search(self, key):
+        pass
+
+# Nearest_Neighbor
+
+def nearest():
+    pass:
+
+# Main and User Interface
 
 if __name__ == "__main__":
+    # Mohmoud Mohamed - 002439508    
     pass
-
 
