f45e64b7aae68de69d4918cc5e2c0dd364a10f67
Hedonium <hedonizing@gmail.com>
2023-12-29 08:16:41 -0500

    Add WGU Hash Table function

--- main.py
+++ main.py
@@ -25,19 +25,6 @@ class Package:
         self.leaving_time = None
         self.delivery_time = None
 
-class Package:
-    def __init__(self, ID, address, city, state, zipcode, Deadline_time, weight, status):
-        self.ID = ID
-        self.address = address
-        self.city = city
-        self.state = state
-        self.zipcode = zipcode
-        self.Deadline_time = Deadline_time
-        self.weight = weight
-        self.status = status
-        self.departure_time = None
-        self.delivery_time = None
-
 
     def __str__(self):
         return f"Package - Address: {self.address}, Deadline: {self.deadline}, City: {self.city}, Zipcode: {self.zipcode}, Weight: {self.weight} kg, Status: {self.status}, Time: {self.time}"
@@ -63,17 +50,6 @@ class Truck:
         self.address = address
         self.leave_time = leave_time
         self.time = leave_time
-
-class Truck:
-    def __init__(self, capacity, speed, load, packages, mileage, address, depart_time):
-        self.capacity = capacity
-        self.speed = speed
-        self.load = load
-        self.packages = packages
-        self.mileage = mileage
-        self.address = address
-        self.depart_time = depart_time
-        self.time = depart_time
 
     def __str__(self):
         return f"Truck ID: {self.truck_id}, Packages: {self.packages}, Miles Driven: {self.miles_driven}, Address: {self.address}, Departure Time: {self.depart_time}"
@@ -122,18 +98,59 @@ truck3 = Truck()
 # Hash Table
 #
 
-class HashTable:
-    def __init__(self):
+ class HashTable:
+    # Constructor with optional initial capacity parameter.
+    # Assigns all buckets with an empty list.
+    def __init__(self, initial_capacity=10):
+        # initialize the hash table with empty bucket list entries.
         self.table = []
-
-    def insert(self, key):
-        pass
-
-    def delete(self, key):
-        pass
-
+        for i in range(initial_capacity):
+            self.table.append([])
+      
+    # Inserts a new item into the hash table.
+    def insert(self, key, item): #  does both insert and update 
+        # get the bucket list where this item will go.
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+ 
+        # update key if it is already in the bucket
+        for kv in bucket_list:
+          #print (key_value)
+          if kv[0] == key:
+            kv[1] = item
+            return True
+        
+        # if not, insert the item to the end of the bucket list.
+        key_value = [key, item]
+        bucket_list.append(key_value)
+        return True
+ 
+    # Searches for an item with matching key in the hash table.
+    # Returns the item if found, or None if not found.
     def search(self, key):
-        pass
+        # get the bucket list where this key would be.
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+        #print(bucket_list)
+ 
+        # search for the key in the bucket list
+        for kv in bucket_list:
+          #print (key_value)
+          if kv[0] == key:
+            return kv[1] # value
+        return None
+ 
+    # Removes an item with matching key from the hash table.
+    def remove(self, key):
+        # get the bucket list where this item will be removed from.
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+ 
+        # remove the item from the bucket list if it is present.
+        for kv in bucket_list:
+          #print (key_value)
+          if kv[0] == key:
+              bucket_list.remove([kv[0],kv[1]])
 
 # Nearest_Neighbor Algorithm
 #
