ac44f579a728ad28be2b7f964ea7cf8cdfd39893
Hedonium <hedonizing@gmail.com>
2023-12-29 02:26:41 -0500

    Fill out main.py skeleton, add package class

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 
+import csv
+
 # Package Class
 
+class Package:
+    def __init__(self, address, deadline, city, zipcode, weight):
+        self.ID = ID
+        self.address = address
+        self.deadline = deadline
+        self.city = city
+        self.zipcode = zipcode
+        self.weight = weight
+        self.status = status
+        self.time = time
+
+    def __str__(self):
+        return f"Package - Address: {self.address}, Deadline: {self.deadline}, City: {self.city}, Zipcode: {self.zipcode}, Weight: {self.weight} kg, Status: {self.status}, Time: {self.time}"
 
 # Truck Class
 
+class Truck:
+    pass
 
-# Hash Table Lookup
+# Hash Table
 
 
-# Status Interface
+
+# User Interface
 
 
